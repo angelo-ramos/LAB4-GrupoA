f46c5480ae880271e17a44ebe6556c4eae03a2ac
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-27 20:49:15 -0500

    dto

--- src/main/java/com/example/lab4grupoa/Dto/ListarMascotasDto.java
+++ src/main/java/com/example/lab4grupoa/Dto/ListarMascotasDto.java
@@ -2,6 +2,7 @@ package com.example.lab4grupoa.Dto;
 
 public interface ListarMascotasDto {
     //nombre, anho, sexo, 'raza', 'servicios'
+    String getIdmascota();
     String getNombre();
     String getAnho();
     String getSexo();
